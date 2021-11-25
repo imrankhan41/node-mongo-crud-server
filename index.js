4a9b747103f70e2268c7f593f45c2fd4168d6123
imrankhanf41 <imranbutex41@gmail.com>
2021-11-25 18:32:27 +0600

    s

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irhuk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 async function run(){
     try{
         await client.connect();
